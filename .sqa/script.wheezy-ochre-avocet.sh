abdb52f6576ffd088ed7c7a90669a0cc16cb3715
Pablo Orviz <orviz@ifca.unican.es>
2025-11-05 10:26:39 +0100

    Add JePL file structure

--- .sqa/script.wheezy-ochre-avocet.sh
+++ .sqa/script.wheezy-ochre-avocet.sh
@@ -4,6 +4,6 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 (
-cd github.com/madgeek-arc/resource-catalogue &&
+cd github.com/madgeek-arc/resource-catalogue-docs &&
     git rev-parse --is-inside-work-tree 
 )
